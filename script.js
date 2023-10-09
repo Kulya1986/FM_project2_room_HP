624d5febcb7af60badda27c08069d6dc54a8fef2
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2023-10-09 10:38:15 +0300

    Changed carousel controls function

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ var rightArr = document.querySelector("#carousel-controls #right-arrow");
 var menuMobile = document.querySelector("#mobile-menu");
 var closeMobile = document.querySelector("#mobile-close");
 
+
 function moveSlide(event){
     var imgSlides = document.querySelectorAll("#carousel > div");
     var copySlides = document.querySelectorAll("#right-section .slide");
 
-   
-
-    for(i=0; i < imgSlides.length; i++ ){
+    for(var i=0; i < imgSlides.length; i++ ){
         var classAttr = imgSlides[i].getAttribute("class");
-        console.log(classAttr.toString());
+        
         if (classAttr.includes("active")) {
             if (event.target.id === "right-arrow" && (i+1)<imgSlides.length)
             {
@@ -24,8 +23,6 @@ function moveSlide(event){
                 copySlides[i].classList.add("disabled");
                 copySlides[i+1].classList.remove("disabled");
                 copySlides[i+1].classList.add("active");
-                console.log(imgSlides[i].classList);
-                console.log(imgSlides[i+1].classList);
                 break;
             }else if (event.target.id === "right-arrow" && (i+1)===imgSlides.length){
                 imgSlides[i].classList.remove("active");
@@ -62,6 +59,27 @@ function moveSlide(event){
     }
 }
 
+function triggerMoveSLide(event){
+    const eventEnter = new KeyboardEvent('keydown', {
+        key: 'Enter',
+        code: 'Enter',
+        which: 13,
+        keyCode: 13,
+      });
+
+    if (event.key === "Enter"){
+        moveSlide(event);
+    } else if (event.key === "ArrowLeft"){
+        leftArr.focus(); 
+          // dispatch the event on some DOM element
+        leftArr.dispatchEvent(eventEnter);
+    } else if (event.key === "ArrowRight"){
+        rightArr.focus(); 
+          // dispatch the event on some DOM element
+        rightArr.dispatchEvent(eventEnter);
+    }
+}
+
 function openMenuMobile(){
     var navigation = document.querySelector("nav");
     navigation.style.backgroundColor = "hsl(0, 0%, 100%)";
@@ -83,5 +101,8 @@ function closeMenuMobile(){
 
 leftArr.addEventListener("onmousedown", moveSlide);
 rightArr.addEventListener("onmousedown", moveSlide);
+leftArr.addEventListener("onkeydown", triggerMoveSLide);
+rightArr.addEventListener("onkeydown", triggerMoveSLide);
+
 menuMobile.addEventListener("click", openMenuMobile)
 closeMobile.addEventListener("click", closeMenuMobile)
